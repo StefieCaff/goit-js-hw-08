c41c5635dfc15cfae8a6e68ddec192c86c937187
StefieCaff <108297341+StefieCaff@users.noreply.github.com>
2023-01-13 13:35:24 -0800

    js code for simplelighbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,8 +1,38 @@
 
-
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const gallery = document.querySelector(".gallery");
+
+//gallery markup from gallery items Array of image objects
+const galleryMarkup = galleryItems.map((image) =>
+  `<a class="gallery__item" href=${image.original}>
+     <img class="gallery__image" src=${image.preview} alt=${image.description} />
+   </a>`
+).join("");
+
+//add gallery html into dom
+gallery.insertAdjacentHTML("beforeEnd", galleryMarkup);
+
+//function to delegate click to div through bubbling when any image is clicked also method to prevent default link behavior
+function selectedImageLibraryModal(event) {
+  event.preventDefault();
+    
+  if (event.target.nodeName !== `A`) {
+    return;
+  }
+};
+
+// initialize simple lightbox image library
+let imageLibraryModal = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
+
+
+//add click event for div/img delegation 
+gallery.addEventListener("click", selectedImageLibraryModal);
