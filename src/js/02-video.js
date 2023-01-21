fd7ee52d3762bf220d3262d53e4dd64c0a15d204
StefieCaff <108297341+StefieCaff@users.noreply.github.com>
2023-01-20 18:39:20 -0800

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,7 +1,7 @@
-//import vimeoplayer from '@vimeo/player';
-const player = require("@vimeo/player");
+//import VimeoPlayer from '@vimeo/player';
+const player = require('@vimeo/player');
 //let player = require("@vimeo/player");
 //const throttle = require("lodash.throttle");
 
-  const iframe = document.querySelector('iframe');
-const newPlayer = new Vimeo.Player(iframe);
+const iframe = document.querySelector('iframe');
+const newPlayer = new Vimeo.Player(iframe)
