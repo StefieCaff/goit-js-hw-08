61559fd0f62c04ae13c33a0b45fc902307590d88
StefieCaff <108297341+StefieCaff@users.noreply.github.com>
2023-01-20 18:05:41 -0800

    goit-js-hw-02 task 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -6,7 +6,7 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+//console.log(galleryItems);
 
 
 const gallery = document.querySelector(".gallery");
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,7 @@
+//import vimeoplayer from '@vimeo/player';
+const player = require("@vimeo/player");
+//let player = require("@vimeo/player");
+//const throttle = require("lodash.throttle");
 
+  const iframe = document.querySelector('iframe');
+const newPlayer = new Vimeo.Player(iframe);
